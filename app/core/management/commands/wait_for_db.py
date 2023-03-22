0b685cf3cc96f2eb6f84676f63e4546b8d1e412f
Wendi Kardian <69452468+wendikardian@users.noreply.github.com>
2023-03-22 23:23:54 +0700

    add unit testing for the databases

---
+++ app/core/management/commands/wait_for_db.py
@@ -0,0 +1,15 @@
+"""
+Django command for wait for the database to be available
+
+"""
+
+
+from django.core.management.base import BaseCommand
+
+class Command(BaseCommand):
+    """
+    Django command to wait for database
+    """
+
+    def handle(self, *args, **options):
+        pass
