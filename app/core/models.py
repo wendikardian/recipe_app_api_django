0366ef9f74ad2b8f28a082173f5830a96ed6b340
Wendi Kardian <69452468+wendikardian@users.noreply.github.com>
2023-03-23 13:56:42 +0700

    add costume model for user?

--- app/core/models.py
+++ app/core/models.py
@@ -1,3 +1,30 @@
-from django.db import models
+"""
+Database models.
+"""
 
-# Create your models here.
+from django.db import models
+from django.contrib.auth.models import (
+    AbstractBaseUser, BaseUserManager, PermissionsMixin
+)
+
+
+class UserManager(BaseUserManager):
+    """Manager for user"""
+    def create_user(self, email, password=None, **extra_fields):
+        """Create and save a new user"""
+        user = self.model(email=email, **extra_fields)
+        # Django automaticly hashes the password
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+class User(AbstractBaseUser, PermissionsMixin):
+    """User in the system."""
+    email = models.EmailField(max_length=255, unique = True)
+    name = models.CharField(max_length=255)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    objects = UserManager()
+
+    USERNAME_FIELD = 'email'
