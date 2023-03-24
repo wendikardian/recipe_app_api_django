89249b6344936fe0a71d3479f6d0505078be4ca0
Wendi Kardian <69452468+wendikardian@users.noreply.github.com>
2023-03-24 23:11:31 +0700

    updated

---
+++ app/core/tests/test_admin.py
@@ -0,0 +1,35 @@
+"""Tests for the Django admin modifications"""
+
+
+from django.test import TestCase
+from django.contrib.auth import get_user_model
+from django.urls import reverse
+from django.test import Client
+
+
+class AdminSiteTests(TestCase):
+    """test case for django admin modifications"""
+    # Setup create a client and superuser and a test regularly user
+    def setUp(self):
+        self.client = Client()
+        self.admin_user = get_user_model().objects.create_superuser(
+            email='admin@example.com',
+            password='testpass123'
+        )
+        # force authentication using user that we created before
+        self.client.force_login(self.admin_user)
+        self.user = get_user_model().objects.create_user(
+            email='user@example.com',
+            password='testpass123',
+            name='Test User'
+        )
+
+    # Test to get user
+    def test_user_list(self):
+        """test that if users are listed on page or not !"""
+        # Reverse to get the url for the change list inside the Django App.
+        url = reverse('admin:core_user_changelist')
+        # Using http get request
+        res = self.client.get(url)
+        self.assertContains(res, self.user.name)
+        self.assertContains(res, self.user.email)
