223d8b095a4823317dcd818577c64344f99383c9
Wendi Kardian <69452468+wendikardian@users.noreply.github.com>
2023-03-25 11:37:57 +0700

    adding some django admin

--- app/core/admin.py
+++ app/core/admin.py
@@ -2,12 +2,36 @@
 
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+# To integrates with Django translation system
+from django.utils.translation import gettext_lazy as _
 
 from core import models
+
 
 class UserAdmin(BaseUserAdmin):
     """Define the admin pages for users"""
     ordering = ['id']
     list_display = ['email', 'name']
+    # None is the title
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        (
+            _('Permissions'),
+            {
+                'fields': (
+                    'is_active',
+                    'is_staff',
+                    'is_superuser'
+                )
+            }
+        ),
+        (
+            _('Important dates'), {
+                'fields': ('last_login',)
+            }
+        )
+    )
+    readonly_fields=['last_login']
+
 
 admin.site.register(models.User, UserAdmin)
